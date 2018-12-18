f0d8555751d9c799d114d07ab6de46e93a046e6a
gary-deshayes <deshayesgary@hotmail.fr>
2018-12-18 13:47:04 +0100

    Pagination actualité

--- src/app/api-actualite-eco/api-actualite-eco.component.ts
+++ src/app/api-actualite-eco/api-actualite-eco.component.ts
@@ -12,30 +12,53 @@ export class ApiActualiteEcoComponent implements OnInit {
   pageSize;
   page;
   selectedValue = null;
+  resultatTotal = null;
+  nombrePagePossible = null;
 
+  // Au lancement du composant la première page est appelée, on retourne 10 article par appel et les articles sont affichés
   constructor(private http: HttpClient) {
     this.page = 1;
     this.pageSize = 10;
     this.getNews(this.page, this.pageSize);
-   }
+  }
 
   ngOnInit() {
   }
 
-  getNews(page: string, pageSize: string){
+  // Récupère les articles grâce à la page et le nombre d'articles par appel
+  getNews(page: string, pageSize: string) {
     this.http.get("https://newsapi.org/v2/everything?sources=les-echos&apiKey=330165c9dd764219b001ac944d15fb0a&page=" + page + "&pageSize=" + pageSize)
-    .subscribe((response: Array<Object>) => {
-      console.log(response);
-      this.news = response["articles"];
-      console.log(this.news);
-    });
+      .subscribe((response: Array<Object>) => {
+        this.news = response["articles"];
+        this.resultatTotal = response["totalResults"];
+        this.nombrePagePossible = Math.round(this.resultatTotal / this.pageSize);
+      });
 
   }
-
-  nbPerPageChange(event : Object){
+  
+  // Permet de changer le nombre d'articles disponibles dans l'appel de l'api et rappel l'api
+  nbPerPageChange(event: Object) {
     console.log(event);
     this.pageSize = event;
     this.getNews(this.page, this.pageSize);
+  }
+
+  // Récupère les articles précédents
+  getNewsPrev() {
+    if (this.page > 1) {
+      this.page--;
+      this.getNews(this.page, this.pageSize);
+    }
+
+  }
+
+  // Récupère les articles suivants
+  getNewsNext() {
+    console.log(this.nombrePagePossible);
+    if (this.page < this.nombrePagePossible) {
+      this.page++;
+      this.getNews(this.page, this.pageSize);
+    }
   }
 
 }
