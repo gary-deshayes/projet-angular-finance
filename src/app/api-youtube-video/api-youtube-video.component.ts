a520b7e5f8c1c2d9cc2544c038c22e7d2f039955
Baptiste Rossignol <baptiste.rossignol@hotmail.fr>
2019-02-11 22:19:42 +0100

    Ajout Like 70%

--- src/app/api-youtube-video/api-youtube-video.component.ts
+++ src/app/api-youtube-video/api-youtube-video.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
+import { YoutubeAuthService } from './../youtube-auth.service';
 
 @Component({
   selector: 'app-api-youtube-video',
@@ -14,10 +15,13 @@ export class ApiYoutubeVideoComponent implements OnInit {
   videoId = this.route.snapshot.paramMap.get('id');
   dangerousVideoUrl = 'http://www.youtube.com/embed/' + this.videoId;
   videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
+
+  headers = "";
   
   public videos = [];
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private sanitizer: DomSanitizer) {
+  constructor(private route: ActivatedRoute, private http: HttpClient, private sanitizer: DomSanitizer, private youtubeAuth: YoutubeAuthService) {
+    this.youtubeAuth.getToken();
    }
 
   ngOnInit() {
@@ -28,6 +32,21 @@ export class ApiYoutubeVideoComponent implements OnInit {
     this.http.get("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=" + this.videoId + "&key=" + this.apiKey)
       .subscribe((response: Array<Object>) => {
         this.videos = response["items"];
+      });
+  }
+
+  public postLike(){
+    this.http.post('https://www.googleapis.com/youtube/v3/videos/rate?id='+ this.videoId +'&rating=like&key='+ this.apiKey, this.headers)
+    .subscribe(
+      (val) => {
+          console.log("POST call successful value returned in body", 
+                      val);
+      },
+      response => {
+          console.log("POST call in error", response);
+      },
+      () => {
+          console.log("The POST observable is now completed.");
       });
   }
 }
