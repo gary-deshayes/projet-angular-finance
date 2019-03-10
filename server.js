7da51e7c480e646cf28244d5ae1fc1bdfb73790b
Deshayes gary <gary.deshayesjr@gmail.com>
2019-03-10 18:25:16 +0100

    heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + '/index.html'));
+res.sendFile(path.join('index.html'));
 });
 
 // Start the app by listening on the default Heroku port
